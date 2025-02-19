e0ce825fbb7e49a18068b8bfde14fc1d3b1092cd
CodeSectarian <codesectarian@gmail.com>
2025-02-19 02:10:49 -0500

    added dynamic routes  ids inside each route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,27 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { BlogComponent } from './pages/blog/blog.component';
+import { PostComponent } from './pages/post/post.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { TagComponent } from './pages/tag/tag.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'blog', component: BlogComponent },
+    { 
+        path: 'blog', component: BlogComponent,
+        children: [
+            {
+              path: 'articulo/:id',
+              component: PostComponent,
+            },
+            {
+              path: 'categoria/:id',
+              component: CategoryComponent,
+            },
+            {
+              path: 'etiqueta/:id',
+              component: TagComponent,
+            },
+        ] 
+    },
 ];
