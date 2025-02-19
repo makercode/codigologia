6123e21cc700d08a6448857f05708b98c1f95b27
CodeSectarian <codesectarian@gmail.com>
2025-02-19 18:53:41 -0500

    added shared icons elements

---
+++ src/app/components/shared/icons/sandwich/sandwich.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sandwich',
+  standalone: true,
+  imports: [],
+  templateUrl: './sandwich.component.html',
+  styleUrl: './sandwich.component.scss'
+})
+export class SandwichComponent {
+
+}
