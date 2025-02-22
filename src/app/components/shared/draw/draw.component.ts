2503c9bdd6c3c6b1ffb8e88604dfc09ad87a2962
CodeSectarian <codesectarian@gmail.com>
2025-02-21 21:32:47 -0500

    added interactive draw component

---
+++ src/app/components/shared/draw/draw.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-draw',
+  standalone: true,
+  imports: [],
+  templateUrl: './draw.component.html',
+  styleUrl: './draw.component.scss'
+})
+export class DrawComponent {
+
+}
