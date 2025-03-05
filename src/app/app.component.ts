649a2c357f673e9e7dd115ca219af34039776529
CodeSectarian <codesectarian@gmail.com>
2025-03-04 22:58:45 -0500

    feat: added sidebar fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { HeaderComponent } from './components/layout/header/header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { MenuComponent } from "./components/shared/menu/menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, MatSidenavModule, MatButtonModule, MenuComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'codigologia';
+export class AppComponent implements OnInit {
+  @ViewChild('sidenav') sidenav!: MatDrawer; 
+  title = 'codigologia'
+  isMenuOpen = false
+
+  constructor() {
+  }
+
+  // @ViewChild('sidenav') public sidenav: MatSidenav;
+
+  ngOnInit() {
+  }
+
+  //suscribe to header changes 
+  onHeaderValueChanged(value: boolean) {
+    this.isMenuOpen = value
+    this.sidenav.toggle();
+  }
+
 }
--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output } from '@angular/core';
 import { SandwichComponent } from "../../shared/icons/sandwich/sandwich.component";
 import { DrawComponent } from "../../shared/logo/draw/draw.component";
 
@@ -10,5 +10,13 @@ import { DrawComponent } from "../../shared/logo/draw/draw.component";
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent  {
+  @Input() isMenuOpen: boolean = false;
+  @Output() headerValueChanged = new EventEmitter<boolean>()
 
+  onValueChanged(value: boolean){
+    this.isMenuOpen = value
+    this.headerValueChanged.emit(this.isMenuOpen);
+  }
+
+  
 }
