37108be2ff3ad45cf36f3e87087eb675cdc92856
CodeSectarian <codesectarian@gmail.com>
2025-03-13 20:32:52 -0400

    feat: added example design posts

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatChipsModule} from '@angular/material/chips';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule,MatButtonModule,MatChipsModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss',
 })
