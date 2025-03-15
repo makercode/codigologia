efa940ca22cdc7a31b05aecda51eaa24585261ee
CodeSectarian <codesectarian@gmail.com>
2025-03-15 04:09:06 -0400

    chore: added angular firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"codigologia-codesectarian","appId":"1:1061106265584:web:0776ead1abe5fd12214135","storageBucket":"codigologia-codesectarian.firebasestorage.app","apiKey":"AIzaSyDC30Tv_x0Y-fo7UjqGWuUyEmzRJjgtc2A","authDomain":"codigologia-codesectarian.firebaseapp.com","messagingSenderId":"1061106265584","measurementId":"G-H1FCW7LTFX"})), provideAnalytics(() => getAnalytics()), ScreenTrackingService, provideFirestore(() => getFirestore())]
 };
