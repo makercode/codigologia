1816e6b3904549c7d41645196fa0954c284828f0
CodeSectarian <codesectarian@gmail.com>
2025-06-20 13:08:52 -0400

    edited blog page

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -2,30 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
-import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { BlogService } from './../../services/data/blog.service';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
   imports: [
+    CommonModule, 
     MatCardModule,
     MatButtonModule,
     MatChipsModule,
     FormsModule,
+    RouterModule
   ],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss',
 })
 export class BlogComponent implements OnInit {
-  markdown = `## Cargando contenido...`;
-
-  constructor(private http: HttpClient) {}
+  
+  posts$:any;
+  constructor(private blogService: BlogService) {}
 
   ngOnInit() {
-    this.loadMarkdownContent();
+    
+    this.blogService.getBlogPosts().subscribe({
+      next: (data) => {
+        console.log('Datos recibidos:', data);
+        this.posts$ = data;
+      },
+      error: (err) => console.error('Error:', err)
+    });
+
   }
 
-  loadMarkdownContent() {
-  }
 }
