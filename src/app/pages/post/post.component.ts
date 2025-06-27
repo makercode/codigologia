e4d7a08d98592a3e8ae96f8c551e1d302eb64e96
CodeSectarian <codesectarian@gmail.com>
2025-06-27 02:29:23 -0400

    fix: adding dynamic url in post

--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { MarkdownComponent } from 'ngx-markdown';
 import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-post',
@@ -17,14 +18,28 @@ export class PostComponent {
 
   markdown = `Cargando contenido...`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
-    this.loadMarkdownContent();
+
+    // Obtener la URL completa
+    const fullUrl = this.router.url; 
+    let pathAfterBlog = "";
+    
+    // Extraer la parte después de 'blog'
+    const blogIndex = fullUrl.indexOf('/blog');
+    if (blogIndex !== -1) {
+      pathAfterBlog = fullUrl.substring(blogIndex + '/blog/articulo'.length);
+      // Resultado: '/articulo/secretos-de-planetas-en-el-principito'
+    }
+
+    console.log('Path after blog:', pathAfterBlog);
+  
+    this.loadMarkdownContent(pathAfterBlog);
   }
 
-  loadMarkdownContent() {
-    const markdownUrl = 'https://raw.githubusercontent.com/codesectarian/codigologia-blog/refs/heads/main/blog/2025/06/tu-personalidad-segun-tu-distro.md';
+  loadMarkdownContent(urlSegment:string) {
+    const markdownUrl = `https://raw.githubusercontent.com/codesectarian/codigologia-blog/refs/heads/main/blog/2025/06/${urlSegment}.md`;
     
     this.http.get(markdownUrl, { responseType: 'text' })
       .pipe(
