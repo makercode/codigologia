a83a77f40146f1bc5ca02541c2f8c428b05ec8f1
CodeSectarian <codesectarian@gmail.com>
2025-06-29 23:10:13 -0400

    added contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { CategoryComponent } from './pages/category/category.component';
 import { TagComponent } from './pages/tag/tag.component';
 import { AboutComponent } from './pages/about/about.component';
 import { Portfolio } from './pages/portfolio/portfolio';
+import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -37,6 +38,15 @@ export const routes: Routes = [
             {
             path: '',
             component: Portfolio,
+            },
+        ] 
+    },
+    { 
+        path: 'contacto',
+        children: [
+            {
+            path: '',
+            component: Contact,
             },
         ] 
     },
