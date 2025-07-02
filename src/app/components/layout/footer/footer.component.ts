4aa8e084742aa6327ccdb8177f9b15a556b56b1b
CodeSectarian <codesectarian@gmail.com>
2025-07-02 04:35:58 -0400

    fix: deleted not used imports in modules

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
--- src/app/components/shared/menu/menu.component.ts
+++ src/app/components/shared/menu/menu.component.ts
@@ -1,11 +1,10 @@
 import { Component, Input, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SandwichComponent } from "../icons/sandwich/sandwich.component";
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterModule, SandwichComponent],
+  imports: [RouterModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
