cb8a654d41681145063ddc2b47f2c59b7403e01c
CodeSectarian <codesectarian@gmail.com>
2025-07-05 07:14:03 -0400

    fix: dynamic portfolio

--- src/app/pages/portfolio/portfolio.ts
+++ src/app/pages/portfolio/portfolio.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,6 +12,7 @@ import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-portfolio',
   imports: [
+    CommonModule, 
     MatCardModule,
     MatChipsModule,
     FormsModule,
@@ -21,6 +23,90 @@ import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
   styleUrl: './portfolio.scss'
 })
 export class Portfolio {
+
+  portfoltio = [
+    {
+      title: "Peru Traveler",
+      text: "Ejercicio Angular usando Ovservables RxJs",
+      state: true
+    },
+    {
+      title: "Woo Starsoft",
+      text: "Plugin aplicando patrones para conectar Api Woocommerce y ERP",
+      state: true
+    },
+    {
+      title: "Mano Alzada",
+      text: "Página Wordpress de noticias Wordpress mantenido por más de 6 años",
+      state: true
+    },
+    {
+      title: "Codigología",
+      text: "Blog Angular interactivo y optimizado en ahorro de recursos",
+      state: true
+    },
+    {
+      title: "MYP",
+      text: "Página Wordpress de presentación para laboratorio dermatológico",
+      state: true
+    },
+    {
+      title: "CYR",
+      text: "Página Wordpress de presentación para proveedor eléctrico",
+      state: true
+    },
+    {
+      title: "Toño Nuñez",
+      text: "Página Wordpress de presentación para el artista Toño Nuñez",
+      state: true
+    },
+    {
+      title: "Monica Pasco",
+      text: "Página Wordpress de presentación para la artista Monica Pasco",
+      state: true
+    },
+    {
+      title: "Voces por el Cambio",
+      text: "Página Wordpress de proyecto social Latoniamericano",
+      state: true
+    },
+    {
+      title: "Yaqua",
+      text: "Página de campaña para el aumento de interacciones en redes sociales",
+      state: true
+    },
+    {
+      title: "Barn",
+      text: "Página interactiva para la previsualización de proyectos tipo establos",
+      state: true
+    },
+    {
+      title: "Beauty Boss",
+      text: "Ecommerce Wordpress de cosméticos y productos de belleza",
+      state: true
+    },
+    {
+      title: "Depor Trivia",
+      text: "Cuestionario sencillo de un equipo de fútbol peruano",
+      state: true
+    },
+    {
+      title: "El negociador",
+      text: "Pagina de campaña hecho para facebook para Cristal",
+      state: true
+    },
+    {
+      title: "Guaraná traductor",
+      text: "Pagina interactiva con traductor cifrado César hecho para Guaraná",
+      state: true
+    }
+    /*,
+    {
+      title: "",
+      text: "",
+      state: true
+    }*/
+  ]
   
   customOptions: OwlOptions = {
     loop: false,
@@ -73,9 +159,29 @@ export class Portfolio {
   }
 
   onMouseMove(event: MouseEvent) {
+    
 
-    const target = event.target as SVGSVGElement;
-    const circle = target.querySelector('.movingCircle') as SVGCircleElement;
+    const target = event.target as HTMLElement;
+    let targetHexagon:HTMLElement|null = target;
+    let circle:HTMLElement|null = target.querySelector('.movingCircle');
+
+    if(!circle) {
+      targetHexagon = target.closest(".hexagon")
+      if(targetHexagon){
+        circle = targetHexagon.querySelector('.movingCircle');
+      }
+    }
+    if(targetHexagon && circle) {
+      let topTargetHexagon = targetHexagon.getBoundingClientRect().top;
+      let leftTargetHexagon = targetHexagon.getBoundingClientRect().left;
+      circle = targetHexagon.querySelector('.movingCircle') as HTMLElement;
+
+      circle.style.top = `${event.clientY-topTargetHexagon}px`
+      circle.style.left = `${event.clientX-leftTargetHexagon}px`
+    }
+
+
+    /*
 
     const svgElement = event.currentTarget as SVGSVGElement;
     
@@ -89,28 +195,29 @@ export class Portfolio {
     
     // Actualiza las posiciones del círculo
     circle.setAttribute('cx', svgPoint.x.toString())
-    circle.setAttribute('cy', svgPoint.y.toString())
+    circle.setAttribute('cy', svgPoint.y.toString())*/
   
   }
 
   onMouseLeave(event: MouseEvent) {
-    const target = event.target as SVGSVGElement;
-    const circle = target.querySelector('.movingCircle') as SVGCircleElement;
-  
-    // Activa la transición suave
-    this.renderer.setStyle(
-      circle,
-      'transition',
-      'cx 0.25s cubic-bezier(0.25, 0.1, 0.25, 1), cy 0.25s cubic-bezier(0.25, 0.1, 0.25, 1)'
-    );
-  
-    circle.setAttribute('cx', "0")
-    circle.setAttribute('cy', "0")
+    /*
+    const target = event.target as HTMLElement;
+    let targetHexagon:HTMLElement|null = target;
+    let circle:HTMLElement|null = target.querySelector('.movingCircle');
 
-    // Elimina la transición después de 0.25s (sin esperar a que termine la animación)
-    setTimeout(() => {
-      this.renderer.setStyle(circle, 'transition', 'none');
-    }, 250); // 250ms = 0.25s
-  
+    if(!circle) {
+      targetHexagon = target.closest(".hexagon")
+      if(targetHexagon){
+        circle = targetHexagon.querySelector('.movingCircle');
+      }
+    }
+    if(targetHexagon && circle) {
+      circle = targetHexagon.querySelector('.movingCircle') as HTMLElement;
+
+      circle.style.top = `-150px`
+      circle.style.left = `-150px`
+
+    }
+    */
   }
 }
