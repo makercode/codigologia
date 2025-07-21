4e9d7e1373420d77d48e3ce0a831ae752bef15f9
CodeSectarian <codesectarian@gmail.com>
2025-07-21 12:10:00 -0400

    add: project svg map info

--- src/app/pages/projects/peru-traveler/peru-traveler.component.ts
+++ src/app/pages/projects/peru-traveler/peru-traveler.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-peru-traveler',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatChipsModule, ],
   templateUrl: './peru-traveler.component.html',
   styleUrl: './peru-traveler.component.scss'
 })
 export class PeruTravelerComponent {
-
+  goExternal(url:string) {
+    window.open(url, '_blank');
+  }
 }
