a44a4bc202368acf2eb1973df2d4c5ae7c607926
CodeSectarian <codesectarian@gmail.com>
2025-08-03 22:11:05 -0400

    test: remove deprecated imports

--- src/app/pages/post/post.component.spec.ts
+++ src/app/pages/post/post.component.spec.ts
@@ -1,10 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PostComponent } from './post.component';
 import { CommonModule } from '@angular/common';
 import { SafeResourcePipe } from '../../pipes/safe-resource-pipe';
-import { MarkdownComponent } from 'ngx-markdown';
+import { MarkdownComponent, MarkdownModule, MarkdownService } from 'ngx-markdown';
 import { provideRouter } from '@angular/router';
+import { BlogService } from '../../services/data/blog.service';
+
+// Mock de BlogService
+class MockBlogService {
+  getStoredBlogPost = jasmine.createSpy('getStoredBlogPost')
+    .and.returnValue(Promise.resolve({
+      markdown: 'assets/markdown/sample.md',
+      title: 'Test Post'
+    }));
+}
+
+// Mock de MarkdownService
+class MockMarkdownService {
+  compile = jasmine.createSpy('compile').and.returnValue('compiled markdown');
+}
 
 describe('PostComponent', () => {
   let component: PostComponent;
@@ -13,16 +27,17 @@ describe('PostComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        PostComponent,
+        PostComponent, // Componente standalone
         CommonModule,
         SafeResourcePipe,
-        MarkdownComponent
+        MarkdownComponent,
       ],
       providers: [
-        provideRouter([])
+        provideRouter([]),
+        { provide: BlogService, useClass: MockBlogService },
+        { provide: MarkdownService, useClass: MockMarkdownService } // Mock del servicio
       ]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PostComponent);
     component = fixture.componentInstance;
