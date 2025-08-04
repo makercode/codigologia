218a9273107767f37af3722a79fa77a85f5ce738
CodeSectarian <codesectarian@gmail.com>
2025-08-03 20:09:01 -0400

    test: fix basic tests in blog component

--- src/app/pages/blog/blog.component.spec.ts
+++ src/app/pages/blog/blog.component.spec.ts
@@ -1,6 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BlogComponent } from './blog.component';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { FormsModule } from '@angular/forms';
+import { BlogService } from '../../services/data/blog.service';
+import { Firestore } from '@angular/fire/firestore';
+import { of } from 'rxjs';
+import { provideRouter, RouterModule } from '@angular/router';
+// import { provideRouter } from '@angular/router';
+
+// Mock de Firestore
+const firestoreMock = {
+  collection: jasmine.createSpy('collection').and.returnValue({
+    // Simula collectionData que retorna un Observable
+    collectionData: jasmine.createSpy('collectionData').and.returnValue(of([]))
+  })
+};
+
+// Mock de BlogService
+class MockBlogService {
+  getStoredBlogPosts = jasmine.createSpy('getStoredBlogPosts').and.returnValue(Promise.resolve([]));
+}
 
 describe('BlogComponent', () => {
   let component: BlogComponent;
@@ -8,7 +30,20 @@ describe('BlogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BlogComponent]
+      imports: [
+        BlogComponent,
+        CommonModule, 
+        MatCardModule,
+        MatButtonModule,
+        MatChipsModule,
+        FormsModule,
+        RouterModule,
+      ],
+      providers: [
+        provideRouter([]),
+        { provide: Firestore, useValue: firestoreMock },
+        { provide: BlogService, useClass: MockBlogService }
+      ]
     })
     .compileComponents();
 
@@ -20,4 +55,12 @@ describe('BlogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize with empty posts', async () => {
+    await fixture.whenStable();
+    expect(component.posts$).toEqual([]);
+  });
+
+  
+  
 });
