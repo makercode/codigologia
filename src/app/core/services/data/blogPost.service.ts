33fe6ad3c678192c916a6641701550b9964d2143
CodeSectarian <codesectarian@gmail.com>
2025-08-08 02:07:16 -0400

    feat: upgrading to clean code naming

--- src/app/core/services/data/blogPost.service.ts
+++ src/app/core/services/data/blogPost.service.ts
@@ -1,20 +1,41 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
-import { firstValueFrom, map, Observable } from 'rxjs';
 import { BlogPost } from '../../domain/blogPost.model';
 import { BlogPostRepository } from '../../repositories/blogPost.repository';
-import { FirebaseBlogPostRepository } from '../../repositories/firebase.blogPost.repository';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
-  constructor(private firestore: Firestore, private blogPostRepository: BlogPostRepository) {}
+  // Firebase repository is configured in app.config.ts injected by BlogPostRepository
+  constructor(private blogPostRepository: BlogPostRepository) {}
 
   storedBlogPosts:BlogPost[] = [];
 
-  async getBlogPosts(): Promise<BlogPost[]> {
+  // public
+
+  public async getStoredBlogPosts() {
+    console.log("called getStoredBlogPosts")
+    if( !this.isBlogPostLocalStorageSetted() ) {
+      console.log("compile storedBlogPosts", localStorage.getItem('storedBlogPosts'))
+      await this.setLocalStorageBlogPosts()
+      return this.getLocalStorageBlogPosts()
+    }
+    console.log("show storedBlogPosts", localStorage.getItem('storedBlogPosts'))
+    return this.getLocalStorageBlogPosts()
+  }
+
+  public async getStoredBlogPost(slug:string) {
+    if( !this.isBlogPostLocalStorageSetted( )) {
+      await this.setLocalStorageBlogPosts()
+    }
+    let posts = await this.getStoredBlogPosts()
+    let post = posts.find((post: BlogPost) => post.slug === slug)
+    return post
+  }
+
+  // Private
+
+  private async getBlogPosts(): Promise<BlogPost[]> {
     console.log("called getBlogPosts");
     
     try {
@@ -27,52 +48,14 @@ export class BlogService {
     }
   }
 
-  async storeBlogPosts() {
-    console.log("called storeBlogPosts")
-    if( !this.isBlogPostStored() ) {
-
+  private async setLocalStorageBlogPosts() {
+    if( !this.isBlogPostLocalStorageSetted() ) {
       let blogPosts = await this.getBlogPosts();
       localStorage.setItem('storedBlogPosts', JSON.stringify(blogPosts))
-
-      /*
-      this.getBlogPosts().subscribe({
-        next: (data) => {
-          console.log('Datos recibidos:', data);
-          // this.posts$ = data;
-          localStorage.setItem('storedBlogPosts', JSON.stringify(data))
-        },
-        error: (err) => console.error('Error:', err)
-      });
-      */
-      
     }
   }
 
-  async getStoredBlogPosts() {
-    console.log("called getStoredBlogPosts")
-    if( !this.isBlogPostStored() ) {
-      console.log("compile storedBlogPosts", localStorage.getItem('storedBlogPosts'))
-      await this.storeBlogPosts()
-      return this.getLocalStorageBlogPosts()
-    }
-    console.log("show storedBlogPosts", localStorage.getItem('storedBlogPosts'))
-    return this.getLocalStorageBlogPosts()
-  }
-
-  
-
-  async getStoredBlogPost(slug:string) {
-    if( !this.isBlogPostStored( )) {
-      await this.storeBlogPosts()
-    }
-    let posts = await this.getStoredBlogPosts()
-    let post = posts.find((post: BlogPost) => post.slug === slug)
-    return post
-  }
-
-
-
-  getLocalStorageBlogPosts() {
+  private getLocalStorageBlogPosts() {
     let storedData = localStorage.getItem('storedBlogPosts');
     if(storedData){
       return JSON.parse(storedData);
@@ -81,8 +64,7 @@ export class BlogService {
     }
   }
 
-  isBlogPostStored():Boolean {
-    console.log("called isBlogPostStored")
+  private isBlogPostLocalStorageSetted():Boolean {
     if(localStorage.getItem('storedBlogPosts')) {
       return true
     }
