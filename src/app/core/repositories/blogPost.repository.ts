c0dceed0e4baae8ab0f2d54b9363ca342e81e240
CodeSectarian <codesectarian@gmail.com>
2025-08-09 01:23:52 -0400

    feat: comment non used method

--- src/app/core/repositories/blogPost.repository.ts
+++ src/app/core/repositories/blogPost.repository.ts
@@ -4,8 +4,4 @@ import { BlogPost } from '../domain/blogPost.model';
 export abstract class BlogPostRepository {
   abstract getBlogPosts(): Promise<BlogPost[]>;
   // abstract getBlogPostBySlug(slug: string): Promise<BlogPost | null>;
-  // Agregar otros métodos según necesidad:
-  // createBlogPost(post: BlogPost): Promise<BlogPost>;
-  // updateBlogPost(id: string, post: Partial<BlogPost>): Promise<BlogPost>;
-  // deleteBlogPost(id: string): Promise<void>;
 }
